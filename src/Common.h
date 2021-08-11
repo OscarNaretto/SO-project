521628c6ffdd545eb400336c696e8c1759c696ee
“Oscar <oscar.naretto@edu.unit.it>
2021-08-11 10:01:04 +0200

    project setup; common.h libs

---
+++ src/Common.h
@@ -0,0 +1,37 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/sem.h>
+
+
+
+
+
+
+#define TEST_ERROR    if (errno) {fprintf(stderr, \
+					   "%s:%d: PID=%5d: Error %d (%s)\n",\
+					   __FILE__,\
+					   __LINE__,\
+					   getpid(),\
+					   errno,\
+					   strerror(errno));exit(-1);}
+
+void allocation_error(char *file, char *structure);
+
+#endif
