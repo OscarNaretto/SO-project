74495679fecc9f52ef9de56a5d9a2fb920c606ad
Vincenzo Milordo <vincenzomilordo@Air-di-Vincenzo.homenet.telecomitalia.it>
2021-08-12 15:21:09 +0200

    struttura base taxi

--- src/Taxi.c
+++ src/Taxi.c
@@ -1,1 +1,19 @@
 #include "Common.h"
+#define EXIT_FAILURE_CUSTOM -1
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+}
+
+void taxi_signal_actions(){
+    /* code */
+}
+
+void signal_handler(int signal){
+    /* code */
+}
+
+void set_maps(){
+    /* code */
+}
